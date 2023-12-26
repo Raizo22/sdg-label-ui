f8f2dd59084af9a445d60a52c7cbb379b499ad9d
Raizo22 <101512802+Raizo22@users.noreply.github.com>
2023-12-26 23:38:40 +0700

    Update projek2.py

--- projek2.py
+++ projek2.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-import re
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def set_custom_style():
+    # Mengatur tampilan CSS
     st.markdown(
         """
         <style>
@@ -26,114 +26,98 @@ def set_custom_style():
             margin-bottom: 20px;
             font-size: 36px; /* Ukuran judul */
         }
-        .btn {
-            display: block;
-            width: 120px;
-            margin: 0 auto;
-            margin-top: 20px;
-            padding: 12px;
-            text-align: center;
-            background-color: #008080; /* Warna tombol */
-            color: white;
-            border: none;
-            border-radius: 8px;
-            cursor: pointer;
-            text-decoration: none;
-            font-weight: bold;
-        }
-        .btn:hover {
-            background-color: #006666; /* Warna tombol saat hover */
-        }
-        .sidebar .sidebar-content {
-            background-color: #dcdcdc; /* Warna sidebar */
-            padding: 10px;
-            border-radius: 10px;
-        }
+        /* Tambahkan gaya tambahan di sini sesuai kebutuhan */
         </style>
         """,
         unsafe_allow_html=True,
     )
 
-def highlight_keyword(text, keyword):
-    pattern = re.compile(re.escape(keyword), re.IGNORECASE)
-    split_text = pattern.split(text)
-    highlighted_text = pattern.sub(lambda match: f"**{match.group()}**", text)
-    return highlighted_text
+def show_bar_plot(dataframe):
+    # Fungsi untuk menampilkan barplot jumlah SDGs yang banyak dicari
+    st.subheader("Barplot Jumlah SDGs yang Banyak Dicari")
+    st.markdown("Grafik ini menunjukkan jumlah SDGs yang banyak dicari dalam DataFrame.")
+    
+    sdg_counts = dataframe['SDG'].value_counts()
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=sdg_counts.index, y=sdg_counts.values)
+    plt.xlabel('SDG')
+    plt.ylabel('Jumlah')
+    plt.xticks(rotation=45)
+    st.pyplot()
 
 def main():
     set_custom_style()
     st.title("Selamat Datang di Aplikasi sdg label")
     st.markdown("---")
-    st.markdown("### Pilih salah satu opsi di bawah ini:")
 
-    # Pilihan untuk memasukkan teks atau file PDF
-    upload_option = st.radio("", ("Masukkan Teks", "Unggah File PDF"))
+    page = st.sidebar.radio("Pilih Halaman:", ("Labelling", "Tren"))
 
-    if upload_option == "Masukkan Teks":
-        text_input = st.text_area("Masukkan Teks Panjang", value="")  # Kotak teks kosong
-        uploaded_file = None
-    else:
-        uploaded_file = st.file_uploader("Unggah File PDF", type="pdf")
-        text_input = None
+    if page == "Labelling":
+        st.title("Halaman Labelling")
+        st.markdown("### Pilih salah satu opsi di bawah ini:")
 
-    if st.button("Kirim"):
-        if text_input:
-            st.success(f"Anda memasukkan teks panjang berikut:\n{text_input}")
-            keyword_to_search = "health"  # Kata kunci yang ingin dicari
-            highlighted_text = highlight_keyword(text_input, keyword_to_search)
-            st.markdown(f"### Bagian yang berkaitan dengan '{keyword_to_search}':")
-            st.markdown(highlighted_text)
-        elif uploaded_file is not None:
-            st.success("Anda telah mengunggah file PDF.")
-            # Operasi dengan file PDF
+        upload_option = st.radio("", ("Masukkan Teks", "Unggah File Excel atau CSV"))
+
+        if upload_option == "Masukkan Teks":
+            text_input = st.text_area("Masukkan Teks Panjang", value="", key="text_input_area")
+            uploaded_file = None
+
+            # Validasi panjang teks yang dimasukkan
+            if text_input.strip():  
+                word_count = len(text_input.split())
+            else:
+                word_count = 0
+
+            if word_count < 50:  # Periksa jumlah kata
+                st.warning("Mohon masukkan minimal 50 kata untuk melanjutkan.")
+                st.text("Jumlah kata yang dimasukkan: " + str(word_count))
+                st.text("Tombol 'Kirim' akan aktif setelah memasukkan minimal 50 kata.")
+                st.button("Kirim", disabled=True)
+            else:
+                st.button("Kirim")
+
+                # Simpan teks dalam DataFrame
+                df = pd.DataFrame({'Text': [text_input]})
+                # Lakukan operasi untuk menentukan label SDGs berdasarkan DataFrame
+                # sdg_label_text = Fungsi_untuk_menentukan_label_SDG(df)
+
         else:
-            st.warning("Harap lengkapi input.")
+            uploaded_file = st.file_uploader("Unggah File Excel atau CSV (satu kolom)", type=["csv", "xlsx"])
+            text_input = None
 
-    # Menampilkan pesan jika pengguna memilih 'Unggah File PDF' tetapi tidak mengunggah file
-    if uploaded_file is None and upload_option == "Unggah File PDF":
-        st.warning("Anda belum mengunggah file PDF. Silakan unggah file PDF untuk melanjutkan.")
-        st.text_area("Masukkan Teks Panjang", "")  # Kotak teks kosong
+            if uploaded_file is not None:
+                try:
+                    # Baca file CSV atau Excel
+                    if uploaded_file.type == 'application/vnd.ms-excel':
+                        df = pd.read_csv(uploaded_file)
+                    else:
+                        df = pd.read_excel(uploaded_file)
 
-    st.sidebar.title("Hal yang Sering Dicari")
+                    st.success("Anda telah berhasil mengunggah file.")
+                    if len(df.columns) == 1:
+                        st.write(df)  # Tampilkan konten file yang diunggah
+                        # Lakukan operasi untuk menentukan label SDGs berdasarkan file CSV atau Excel
+                        # sdg_label_text = Fungsi_untuk_menentukan_label_SDG(df)
+                    else:
+                        st.warning("Mohon unggah file dengan hanya satu kolom data.")
 
-    # Dummy DataFrame
-    data = {
-        'Column1': ['Value1', 'Value2', 'Value3', 'Value4', 'Value5'],
-        'Column2': ['ValueA', 'ValueB', 'ValueC', 'ValueD', 'ValueE']
-    }
-    df = pd.DataFrame(data)
+                    st.markdown("*Catatan: Pastikan file yang diunggah hanya memiliki satu kolom data untuk hasil yang akurat.*")
 
-    if df is not None:
-        for column in df.columns:
-            st.sidebar.markdown(f"#### {column}")
-            for value in df[column].value_counts().head().index.tolist():
-                st.sidebar.markdown(f"- {value}")
+                except Exception as e:
+                    st.warning(f"Terjadi kesalahan dalam membaca file. Pesan kesalahan: {str(e)}")
 
-    st.header("Performa Model (Line Plot)")
+    elif page == "Tren":
+        st.title("Halaman Tren")
+        st.markdown("### Analisis Tren SDGs")
 
-    # Membuat plot performa model (contoh menggunakan data acak)
-    data = {
-        'Epoch': np.arange(1, 11),
-        'Loss': np.random.uniform(0.1, 0.5, 10),
-        'Accuracy': np.random.uniform(0.6, 0.9, 10)
-    }
-    df = pd.DataFrame(data)
+        # Ambil data dari file atau sumber lain
+        # Misalnya, Anda telah memiliki DataFrame df yang berisi data dari file yang diunggah
 
-    fig, ax1 = plt.subplots()
-
-    color = 'tab:red'
-    ax1.set_xlabel('Epoch')
-    ax1.set_ylabel('Loss', color=color)
-    ax1.plot(df['Epoch'], df['Loss'], color=color)
-    ax1.tick_params(axis='y', labelcolor=color)
-
-    ax2 = ax1.twinx()  
-    color = 'tab:blue'
-    ax2.set_ylabel('Accuracy', color=color)  
-    ax2.plot(df['Epoch'], df['Accuracy'], color=color)
-    ax2.tick_params(axis='y', labelcolor=color)
-
-    st.pyplot(fig)
+        # Tampilkan barplot jumlah SDGs yang banyak dicari
+        if 'SDG' in df.columns:  # Pastikan kolom SDG ada dalam DataFrame
+            show_bar_plot(df)
+        else:
+            st.warning("Kolom 'SDG' tidak ditemukan dalam DataFrame.")
 
 if __name__ == "__main__":
     main()
